102ee17158e44039c08b59dec659ff4d8892c8ec
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-06 21:08:15 +0000

    correcion de errores en las funciones para add o delete: giphy o marvel; se han corregido ciertos erroes en los estilos

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -8,72 +8,70 @@
     HomeController.$inject = ["$location","UserLocalProvider","GiphyServerProvider","MarvelServerProvider"];
     function HomeController($location,ULP,GSP,MSP) {
         let vm = this;
-        ///////////////////// VAR SCOPE USER ///////////////////////
+        ////////////////////////// VAR USER ////////////////////////
         vm.usersList = [];
         vm.user = {};
         vm.edit = false;
         vm.showView = 0;
-        ///////////////////// FUCTION SCOPE USER ///////////////////
+        //////////////////////// FUCTION USER //////////////////////
+        vm.resetVar = resetVar;
         vm.addNewUser = addNewUser;
         vm.editUser = editUser;
         vm.modifyUser = modifyUser;
         vm.removeUser = removeUser;
-        ////////////////////// NAV PERFIL USER //////////////////////
+        ////////////////////// NAV PERFIL USER /////////////////////
         vm.go = go;
-        ///////////////////// VAR SCOPE USER ///////////////////////
+        ///////////////////////// VAR GIPHY ////////////////////////
         vm.giphyList = [];
         vm.search_giphy = {};
-        // Recent: true; Trending: false
         vm.typeGiphy = 'search';
-        ///////////////////// FUCTION SCOPE GIPHY //////////////////
-        vm.getGiphies = getGiphies;
-        vm.RecentsTrending = RecentsTrending;
-        ///////////////////////// USER GIPHY ///////////////////////
-        vm.addGifsFav = addGifsFav;
-        vm.deleteGifsFav = deleteGifsFav;
-        vm.checkGiphyUser = checkGiphyUser;
         ///////////////////////// VAR MARVEL ///////////////////////
         vm.marvelListSelect = [];
         vm.search_marvel = {};
         vm.marvelList = [];
         vm.typeMarvel = '';
-        ////////////////////////// FUCTION MARVEL ////////////////////
+        //////////////////////// USER TYPE FAV /////////////////////
+        vm.addTypeFav = addTypeFav;
+        vm.deleteTypeFav = deleteTypeFav;
+        vm.checkTypeUser = checkTypeUser;
+        /////////////////////// FUCTION GIPHY //////////////////////
+        vm.getGiphies = getGiphies;
+        /////////////////////// FUCTION MARVEL /////////////////////
         vm.getMarvel = getMarvel;
-        ////////////////////////// USER MARVEL ///////////////////////
-        vm.addMarvelFav = addMarvelFav;
-        vm.deleteMarvelFav = deleteMarvelFav;
-        vm.checkMarvelUser = checkMarvelUser;
-        ////////////////////////// INIT ////////////////////////////
+        ///////////////////////// INIT /////////////////////////////
         activate();
-        ////////////////////////// FUCTION INIT ////////////////////
+        ///////////////////////// FUCTION INIT /////////////////////
         function activate() {
+            vm.resetVar();
             vm.usersList = ULP.getUsersAll();
             vm.marvelListSelect = MSP.getTypes();
+        };
+        //////////////////////// FUCTION USER //////////////////////
+        function resetVar(){
+            vm.user = {name:'', email:'', phone:'', photo:'', giphy:[], marvel:[]};
             vm.search_giphy = {text:'', type:'', offset:false, direction:false};
             vm.search_marvel = {text:'', type:'', offset:false, direction:false}
-            vm.user.giphy = [];
-            vm.user.marvel = [];
         };
-        ////////////////////////// FUCTION USER ////////////////////
         function addNewUser(){
             vm.user = ULP.addUser(vm.user);
             vm.usersList.push(vm.user);
             vm.formUser.$setPristine();
-            vm.user = {};
+            vm.resetVar();
         };
         function editUser(id){
+            vm.resetVar();
             vm.user = ULP.getUser(id);
             vm.edit = true;
         };
         function modifyUser(){
-            ULP.updateUser(vm.user);
             vm.edit = false;
+            ULP.updateUser(vm.user);
             for (let i = 0; i < vm.usersList.length; i++) {
                 const u = vm.usersList[i];
                 if (u.id == vm.user.id) vm.usersList[i] = vm.user;
             }
             vm.formUser.$setPristine();
-            vm.user = {};
+            vm.resetVar();
         };
         function removeUser(user){
             let userName = prompt("Estas seguro que deseas elimiar el usuario? " + user.name);
@@ -84,67 +82,63 @@
                 }
             }
         };
-        ////////////////////// NAV PERFIL USER //////////////////////
+        ////////////////////// NAV PERFIL USER /////////////////////
         function go(id){
             let url = "/user/" + id;
             $location.path(url);
         };
-        ////////////////////////// FUCTION GIPHY ////////////////////
+        //////////////////////// USER TYPE FAV /////////////////////
+        function addTypeFav(type, data){
+            let isIn = false;
+            let arrayType = (type == 'Giphy') ? vm.user.giphy : vm.user.marvel;
+            for (let i = 0; i < arrayType.length; i++) {
+                if (arrayType[i].id == data.id){
+                    isIn = true;
+                    break;
+                }
+            }
+            if(!isIn){
+                arrayType.push(data);
+                vm.user[type] = arrayType;
+                ULP.updateUser(vm.user);
+            }
+        }
+        function deleteTypeFav(type, data){
+            let arrayType = (type == 'Giphy') ? vm.user.giphy : vm.user.marvel;
+            for (let i = 0; i < arrayType.length; i++) {
+                if (arrayType[i].id == data.id){
+                    arrayType.splice(i,1);
+                    vm.user[type] = arrayType;
+                    ULP.updateUser(vm.user);
+                    break;
+                }
+            }
+        }
+        function checkTypeUser(type, ident){
+            if(!vm.edit) return;
+            let arrayType = (type == 'Giphy') ? vm.user.giphy : vm.user.marvel;
+            for (let i = 0; i < arrayType.length; i++) {
+                if(arrayType[i].id == ident) return true;
+            }
+            return false;
+        }
+        /////////////////////// FUCTION GIPHY //////////////////////
         function getGiphies(offset, direction){
             vm.search_giphy.offset = offset;
             vm.search_giphy.direction = direction;
             vm.search_giphy.type = vm.typeGiphy;
-            if(vm.rectOrTrend) GSP.getGifsType(vm.search_giphy).then(response => vm.giphyList = response).catch(errorFuction);
-            else  GSP.getGifsType(vm.search_giphy).then(response => vm.giphyList = response).catch(errorFuction);
+            if(vm.typeGiphy == 'search') GSP.getGifsType(vm.search_giphy).then(response => vm.giphyList = response).catch(errorFuction);
+            else if(vm.typeGiphy == 'trending') GSP.getGifsType(vm.search_giphy).then(response => vm.giphyList = response).catch(errorFuction);
         };
         function errorFuction(response){
             console.error(response);
         };
-        function RecentsTrending(bool){
-            vm.rectOrTrend = bool;
-        }
-        ////////////////////////// USER GIPHY ///////////////////////
-        function addGifsFav(Giphy){
-            if (!vm.user.giphy.includes(Giphy)) vm.user.giphy.push(Giphy);
-            ULP.updateUser(vm.user);
-        }
-        function deleteGifsFav(Giphy){
-            for (let i = 0; i < vm.user.giphy.length; i++) {
-                if(vm.user.giphy.includes(Giphy)) vm.user.giphy.splice(i,1);
-            }
-            ULP.updateUser(vm.user);
-        }
-        function checkGiphyUser(ident){
-            for (let i = 0; i < vm.user.giphy.length; i++) {
-                const g = vm.user.giphy[i];
-                if(g.id == ident) return true;
-            }
-            return false;
-        }
-        ////////////////////////// FUCTION MARVEL ////////////////////
+        //////////////////////// FUCTION MARVEL /////////////////////
         function getMarvel(offset, direction){
             vm.search_marvel.offset = offset;
             vm.search_marvel.direction = direction;
             vm.search_marvel.type = vm.typeMarvel;
             MSP.getMarvelFct(vm.search_marvel).then(response => vm.marvelList = response).catch(errorFuction);
         };
-        ////////////////////////// USER MARVEL ///////////////////////
-        function addMarvelFav(Marvel){
-            if (!vm.user.marvel.includes(Marvel)) vm.user.marvel.push(Marvel);
-            ULP.updateUser(vm.user);
-        }
-        function deleteMarvelFav(Marvel){
-            for (let i = 0; i < vm.user.marvel.length; i++) {
-                if (vm.user.marvel.includes(Marvel)) vm.user.marvel.splice(i,1);
-            }
-            ULP.updateUser(vm.user);
-        }
-        function checkMarvelUser(ident){
-            for (let i = 0; i < vm.user.marvel.length; i++) {
-                const g = vm.user.marvel[i];
-                if(g.id == ident) return true;
-            }
-            return false;
-        }
     }
 })();
