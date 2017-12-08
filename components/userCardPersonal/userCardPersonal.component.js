ff1441d429d75a25450e6fcc91f3a365d3d4f30e
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-08 12:21:31 +0000

    se ha realizado un nuevo componente para sustituir la colunmna de la izquierda, que compone las tarjetas de usuario que hay en la web, por una directiva y mejorar el código

---
+++ components/userCardPersonal/userCardPersonal.component.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+    // Usage:
+    // 
+    // Creates:
+    // 
+    angular
+        .module('Agend_User')
+        .component('userCardPersonal', {
+            templateUrl: '/components/userCardPersonal/userCardPersonal.html',
+            controller: UserCardPersonalController,
+            controllerAs: '$ctrl',
+            bindings: {
+                usersList: '<',
+                showUser: '&',
+                editUser: '&',
+                removeUser: '&',
+            },
+        });
+    UserCardPersonalController.$inject = [];
+    function UserCardPersonalController() {
+        var $ctrl = this;
+        ////////////////
+        $ctrl.$onInit = function() { };
+        $ctrl.$onChanges = function(changesObj) { };
+        $ctrl.$onDestroy = function() { };
+    }
+})();
